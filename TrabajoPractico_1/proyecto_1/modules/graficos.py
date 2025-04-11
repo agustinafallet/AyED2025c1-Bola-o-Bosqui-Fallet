896c499116c40ce096b3a5b44c4a1e48d79d62c2
agustinafallet <agustinafallet@gmail.com>
2025-04-11 00:31:49 -0300

    no grafica, por tiempo hablaraag

---
+++ TrabajoPractico_1/proyecto_1/modules/graficos.py
@@ -0,0 +1,30 @@
+from matplotlib import pyplot as plt
+from tiempos import medir_tiempos
+from ordenamiento_burbuja import ordenamiento_burbuja
+from ordenamiento_quicksort import ordenamiento_quicksort
+from ordenamiento_residuos import ordenamiento_de_residuo
+
+def graficar_tiempos(lista_metodos_ord):
+    tamanos = [1, 10, 100, 200, 500, 700, 1000]
+    
+    # figsize es el tamaño de la figura en pulgadas (width, height)
+    plt.figure(figsize=(10, 6))
+
+    for metodo_ord in lista_metodos_ord:
+        
+        tiempos = medir_tiempos(metodo_ord, tamanos)
+
+        # plot es para graficar los tiempos de ordenamiento
+        # plot es el método de matplotlib para graficar
+        # marker='o' es para poner un punto en cada coordenada
+        plt.plot(tamanos, tiempos, marker='o', label=metodo_ord.__name__)
+
+    plt.xlabel('Tamaño de la lista')
+    plt.ylabel('Tiempo (segundos)')
+    plt.title('Comparación de tiempos de ordenamiento')
+    plt.legend() # para mostrar el nombre del método de ordenamiento. Es el "label" del metodo plot
+    plt.grid() # cuadriculado
+    plt.savefig('grafico_tiempos.png')
+    plt.show(block=True)
+
+graficar_tiempos([ordenamiento_burbuja, ordenamiento_de_residuo, ordenamiento_quicksort])
---
+++ TrabajoPractico_1/proyecto_1/modules/graficos2.py
@@ -0,0 +1,24 @@
+from matplotlib import pyplot as plt
+from tiempos2 import medir_tiempos
+
+def graficar_tiempos(lista_metodos_ord):
+    tamanos = [1, 10, 100, 300, 500, 700, 1000]
+    
+    # figsize es el tamaño de la figura en pulgadas (width, height)
+    plt.figure(figsize=(10, 6))
+
+    for metodo_ord in lista_metodos_ord:
+        
+        tiempos = medir_tiempos(metodo_ord, tamanos)
+
+        # plot es para graficar los tiempos de ordenamiento
+        # plot es el método de matplotlib para graficar
+        # marker='o' es para poner un punto en cada coordenada
+        plt.plot(tamanos, tiempos, marker='o', label=metodo_ord.__name__)
+
+    plt.xlabel('Tamaño de la lista')
+    plt.ylabel('Tiempo (segundos)')
+    plt.title('Comparación de tiempos de ordenamiento')
+    plt.legend() # para mostrar el nombre del método de ordenamiento. Es el "label" del metodo plot
+    plt.grid() # cuadriculado
+    plt.show()
