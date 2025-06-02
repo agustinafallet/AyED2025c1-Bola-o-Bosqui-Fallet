1568bea3625dd517cf9368aed979002dec24a8ee
marielbosqui <bosquimariel@gmail.com>
2025-06-02 10:53:14 -0300

    p3 casi completo

--- TrabajoPractico_2/proyecto_3/prim.py
+++ TrabajoPractico_2/proyecto_3/prim.py
@@ -15,7 +15,8 @@ def prim(G, inicio):
     while not cp.estaVacio():
         vertice_actual = cp.eliminarMin()[1]
         for vertice_siguiente in vertice_actual.obtener_conexiones():
-            nuevo_costo = vertice_actual.obtener_distancia() + vertice_actual.obtener_ponderacion(vertice_siguiente)            # comienza función de relajación
+            nuevo_costo = vertice_actual.obtener_distancia() + vertice_actual.obtener_ponderacion(vertice_siguiente)
+            # comienza función de relajación
             
             ####prueba de funcionamiento
             # print(f"Evaluando arista {vertice_actual.obtener_id()} -> {vertice_siguiente.obtener_id()} con costo {nuevo_costo}")
@@ -33,15 +34,16 @@ def prim(G, inicio):
 
 
 if __name__ == "__main__":
-    g = Grafo()
-    g.agregar_aristas('A', 'B', 1)
-    g.agregar_aristas('A', 'C', 3)
-    g.agregar_aristas('B', 'C', 1)
-    g.agregar_aristas('B', 'D', 4)
-    g.agregar_aristas('C', 'D', 2)
-    
-    prim(g, g.obtener_vertice('A'))
-    for v in g:
-        print(f"Vertice: {v.obtener_id()}, Predecesor: {v.obtener_predecesor().obtener_id() if v.obtener_predecesor() else None}, Distancia: {v.obtener_distancia()}")
-
-    
+    grafo = Grafo()
+    # Crear el grafo y agregar los vértices
+    aldeas = [['Aldea1', 'Aldea2', 5], ['Aldea1', 'Aldea3', 10], ['Aldea2', 'Aldea3', 2], ['Aldea2', 'Aldea4', 3], ['Aldea3', 'Aldea4', 1]]
+    for aldea in aldeas:
+        grafo.agregar_vertices(aldea[0])
+        grafo.agregar_vertices(aldea[1])
+        grafo.agregar_aristas(aldea[0], aldea[1], aldea[2])
+    # Ejecutar el algoritmo de Prim
+    prim(grafo, grafo.obtener_vertice('Aldea1'))
+    # Imprimir los resultados
+    for v in grafo:
+        predecesor = v.obtener_predecesor().obtener_id() if v.obtener_predecesor() else None
+        print(f"Vértice: {v.obtener_id()}, Predecesor: {predecesor}, Distancia: {v.obtener_distancia()}")
